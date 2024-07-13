5161a560fc5ed34a3c9b49cdb3810e3ec818d1ce
hungdoba <hungdoba.hdb@gmail.com>
2024-07-13 21:35:18 +0900

    update footer logo

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
 import { auth } from '@/auth';
 import { Locale } from '@/i18n-config';
 import Newsletter from '../forms/Newsletter';
 import ThemeSwitch from '../controls/ThemeSwitch';
 import { SignInIcon, SignOutIcon } from '../controls/Icon';
-import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
 import { getDictionary } from '@/get-dictionary';
 
 interface Props {
@@ -19,16 +21,38 @@ export default async function Footer({ lang }: Props) {
       <div className="p-4 mx-auto max-w-screen-xl lg:pt-16 lg:pb-8 lg:px-6">
         <Newsletter dictionary={dictionary} />
       </div>
-      <div className="flex flex-col items-center ">
+      <div className="flex flex-col items-center">
         <div className="mb-3 flex space-x-4">
-          <FaFacebook size={32} className="hover:text-blue-700" />
-          <FaTwitter size={32} className="hover:text-blue-500" />
-          <FaInstagram size={32} className="hover:text-orange-700" />
+          <Link
+            className="hover:cursor-pointer"
+            href={`https://www.facebook.com/asiatips.net`}
+            target="_blank"
+          >
+            <FaFacebook size={32} className="hover:text-blue-700" />
+          </Link>
+
+          <Link
+            className="hover:cursor-pointer"
+            href={`https://x.com/asiatips__net`}
+            target="_blank"
+          >
+            <FaXTwitter size={32} className="hover:text-blue-500" />
+          </Link>
+
+          <Link
+            className="hover:cursor-pointer"
+            href={`https://www.instagram.com/asiatips_net`}
+            target="_blank"
+          >
+            <FaInstagram size={32} className="hover:text-orange-700" />
+          </Link>
         </div>
-        <div className="mb-2 flex space-x-2 text-sm ">
+        <div className="mb-2 flex flex-row items-center space-x-2 text-sm">
           <div>{` • `}</div>
           <div>Asiatips.net</div>
-          <ThemeSwitch />
+          <div>
+            <ThemeSwitch />
+          </div>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           {session ? <SignOutIcon /> : <SignInIcon />}
@@ -36,7 +60,6 @@ export default async function Footer({ lang }: Props) {
           <div>All rights reserved</div>
           <div>{` • `}</div>
         </div>
-        <div className="mb-8 text-sm "></div>
       </div>
     </footer>
   );
