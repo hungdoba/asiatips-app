b03c3519875e019f7e5ee1586c093013d4fbb7ea
hungdoba <hungdoba.hdb@gmail.com>
2024-07-15 10:52:00 +0900

    SEO fix warning li

--- components/controls/LocaleSwitcher.tsx
+++ components/controls/LocaleSwitcher.tsx
@@ -68,8 +68,7 @@ export default function LocaleSwitcher() {
                   className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
                   href={changeLocaleInUrlPath(pathName, locale.locale)}
                 >
-                  {/* <Option option={locale} /> */}
-                  {locale.locale}
+                  <Option option={locale} />
                 </Link>
               </li>
             ))}
