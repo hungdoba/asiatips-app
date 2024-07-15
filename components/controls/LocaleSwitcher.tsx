b08e4582b537e08c380f33c4da83286ed6db3408
hungdoba <hungdoba.hdb@gmail.com>
2024-07-15 10:39:21 +0900

    SEO update li in language options

--- components/controls/LocaleSwitcher.tsx
+++ components/controls/LocaleSwitcher.tsx
@@ -63,7 +63,7 @@ export default function LocaleSwitcher() {
           {locales
             .filter((locale) => locale.locale !== currentLocale)
             .map((locale, index) => (
-              <li key={index} role="none">
+              <li key={index} role="presentation">
                 <Link
                   className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
                   href={changeLocaleInUrlPath(pathName, locale.locale)}
