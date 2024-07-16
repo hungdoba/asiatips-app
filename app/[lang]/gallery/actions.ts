3a7f39efac0dc204c6542df714a3de1836846099
hungdoba <hungdoba.hdb@gmail.com>
2024-07-16 17:56:54 +0900

    update actions for gallery

--- app/[lang]/gallery/actions.ts
+++ app/[lang]/gallery/actions.ts
@@ -21,64 +21,6 @@ export async function getImagesCount(): Promise<number> {
   }
 }
 
-// export async function getAllImages(): Promise<CloudImage[]> {
-//   try {
-//     const results = await cloudinary.search
-//       .expression(
-//         `folder:${process.env.NEXT_PUBLIC_CLOUDINARY_GALLERY_FOLDER}/*`
-//       )
-//       .sort_by('public_id', 'desc')
-//       .execute();
-
-//     const cleanResult: CloudImage[] = results.resources.map(
-//       async (result: any, index: number) => ({
-//         id: index,
-//         height: result.height,
-//         width: result.width,
-//         aspect_ratio: result.aspect_ratio,
-//         public_id: result.public_id,
-//         format: result.format,
-//         blur_data_url: await getBlurDataUrl(result.public_id, result.format),
-//       })
-//     );
-//     return cleanResult;
-//   } catch (error) {
-//     console.error('Error fetching image thumbnails:', error);
-//     return [];
-//   }
-// }
-
-// export async function getBlurDataUrl(
-//   public_id: string,
-//   format: string
-// ): Promise<string> {
-//   const response = await fetch(
-//     `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_jpg,w_8,q_70/${public_id}.${format}`
-//   );
-//   const buffer = await response.arrayBuffer();
-//   const minified = await imagemin.buffer(Buffer.from(buffer));
-
-//   let url = `data:image/jpeg;base64,${Buffer.from(minified).toString(
-//     'base64'
-//   )}`;
-//   return url;
-// }
-
-export async function getBackgroundBlurImage(
-  image: CloudImage
-): Promise<string> {
-  const response = await fetch(
-    `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_jpg,w_8,q_70/${image.public_id}.${image.format}`
-  );
-  const buffer = await response.arrayBuffer();
-  const minified = await imagemin.buffer(Buffer.from(buffer));
-
-  let url = `data:image/jpeg;base64,${Buffer.from(minified).toString(
-    'base64'
-  )}`;
-  return url;
-}
-
 export async function getAllImages(): Promise<CloudImage[]> {
   try {
     const results = await cloudinary.search
@@ -99,8 +41,6 @@ export async function getAllImages(): Promise<CloudImage[]> {
         blur_data_url: await getBlurDataUrl(result.public_id, result.format),
       }))
     );
-
-    cleanResult.map((a) => console.log(`'${a.blur_data_url}',`));
 
     return cleanResult;
   } catch (error) {
