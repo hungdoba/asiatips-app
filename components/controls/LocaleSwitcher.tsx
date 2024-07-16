4e400fce86a5c943d8f448b61912ad26463deef2
hungdoba <hungdoba.hdb@gmail.com>
2024-07-16 18:10:04 +0900

    fix bug locale selector in mobile mode

--- components/controls/LocaleSwitcher.tsx
+++ components/controls/LocaleSwitcher.tsx
@@ -52,10 +52,10 @@ export default function LocaleSwitcher() {
         />
       </button>
       <div
-        className={`w-full absolute top-full mt-2 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 transform transition-transform duration-300 ${
+        className={`w-full z-10 absolute top-full mt-2 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 transform transition-transform duration-300 ${
           isDropdownOpen
             ? 'translate-y-0 ease-out opacity-100'
-            : 'translate-y-4 ease-in opacity-0'
+            : 'hidden translate-y-4 ease-in opacity-0'
         }`}
         id="language-dropdown-menu"
       >
