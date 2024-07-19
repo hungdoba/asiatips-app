b753337103a28b61b4adf8e74368ae583e213101
hungdoba <hungdoba.hdb@gmail.com>
2024-07-19 21:17:53 +0900

    SEO main image view

--- app/[lang]/gallery/[id]/layout.tsx
+++ app/[lang]/gallery/[id]/layout.tsx
@@ -11,13 +11,10 @@ export default async function Layout({
         <Image
           className="pointer-events-none h-full w-full"
           src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEAlgCWAAD/2wBDAAoHBwgHBgoICAgLCgoLDhgQDg0NDh0VFhEYIx8lJCIfIiEmKzcvJik0KSEiMEExNDk7Pj4+JS5ESUM8SDc9Pjv/2wBDAQoLCw4NDhwQEBw7KCIoOzs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozv/wAARCAAFAAgDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAT/xAAdEAACAgIDAQAAAAAAAAAAAAABAgAFAxEEEiHB/8QAFQEBAQAAAAAAAAAAAAAAAAAAAAT/xAAZEQEBAQADAAAAAAAAAAAAAAABAgADEUH/2gAMAwEAAhEDEQA/AIKC1NXSDkLhGUs4Xq7HXp19iIiOW5EH3T3JT27/2Q=="
+          priority={true}
           alt="Blurred background"
-          // fill
-          // sizes="100vw"
-          width={1200} // Set your desired width
-          height={800} // Set your desired height to maintain aspect ratio
-          sizes="(max-width: 600px) 480px, (max-width: 1200px) 800px, 1200px"
-          priority={true} // Use fetchpriority="high" equivalent
+          fill
+          sizes="100vw"
         />
       </div>
       {children}
