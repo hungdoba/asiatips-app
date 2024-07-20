ab9aa4f45fc9f216bf543a53c83fd9433d54b315
hungdoba <hungdoba.hdb@gmail.com>
2024-07-20 13:11:46 +0900

    update navbar order

--- actions/no-cache/category.ts
+++ actions/no-cache/category.ts
@@ -4,6 +4,9 @@ import prisma from '@/lib/prisma';
 export async function getCategories() {
   try {
     const categories = await prisma.post_category.findMany({
+      orderBy: {
+        id: 'asc',
+      },
       where: {
         active: true,
       },
